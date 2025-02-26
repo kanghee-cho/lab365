ba800441a019e0d89e9a000ecde34a5b0cb67d35
Kanghee (Joe) Cho <khcho.me@outlook.com>
2025-02-26 16:33:25 +0900

    up django

--- src/lbrx/lbrx/settings.py
+++ src/lbrx/lbrx/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rs3bx^+o0bdlj_da)!b!a831%eg9)+inl$!cfr1$wg%@hbs+@r'
+with open("./secrets.json") as f:
+    secrets = json.loads(f.read())
+SECRET_KEY = secrets['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
